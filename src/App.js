76d8552881eb048d36634faaad65ed2600956781
Collyngodlewski <collyngodlewski@gmail.com>
2021-10-12 19:05:00 -0400

    internet was out most of the day, just started. pushing so i can submit and keep working.

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import NameForm from './Form';
+
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+}
+
+
+
+export default function App() {
+  const [names, setNames] = useState([])
+  
+  
+  
+  
+  
+  
+  
+  
+  return (
+    <div className="App">
+      <h2>Hello World!</h2>
+      <NameForm
+      
+      
+      
+      />
+    </div>
+  );
+}
+
+ 
